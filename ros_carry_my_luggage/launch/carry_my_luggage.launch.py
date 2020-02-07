a3c9a12b80d461822b69f5ddab40680b65d478c6
ItoMasaki <l1sum@icloud.com>
2020-02-07 13:02:38 +0900

    add launch file

---
+++ ros_carry_my_luggage/launch/carry_my_luggage.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+import launch_ros.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package="turtlebot_bringup",
+            node_executable="turtlebot2",
+            output="screen"
+        ),
+
+        launch_ros.actions.Node(
+            package="ydlidar",
+            node_executable="ydlidar_node",
+            output="screen"
+        ),
+    ])
